5abdae7b760baa1f22a55d66ac82b72b5e4db4b4
Alex Mariakakis <atm15@duke.edu>
2012-02-20 20:26:44 -0500

    fixed main

--- src/Main.java
+++ src/Main.java
@@ -20,32 +20,6 @@ import filter.*;
 import html_generator.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		
-		// Choose the file to parse
-		Scanner in = new Scanner(System.in);
-		System.out.println("Please select a file to parse (don't include .xml)");
-		String xmlFile = in.nextLine();
-		ArrayList<CalendarEvent> list = Parser.parseFile(xmlFile + ".xml");
-		
-		// Choose the method of filtering
-		System.out.println("Please select an option (keyword or date):");
-		String filterOption = in.nextLine();
-		
-		Map<Object, ArrayList<CalendarEvent>> outputMap = new TreeMap<Object, ArrayList<CalendarEvent>>();
-		if (filterOption.equals("keyword"))
-			outputMap = Filter.filterByKeyword(list, "Bears");
-		else if (filterOption.equals("date"))
-			outputMap = Filter.filterByDates(list, new DateTime(2010, 11, 11, 0, 0), new DateTime(2012, 12, 1, 23, 0));
-		
-		// Choose the method of output
-		System.out.println("Please select the type of output you would like (vertical or horizontal):");
-		String outputOption = in.nextLine();
-		
-		HtmlGenerator tableCreator = new HtmlHorizontalTable(outputMap);
-		HtmlGenerator headerCreator = new HtmlVerticalHeaders(outputMap);
-
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException, JDOMException {
 
