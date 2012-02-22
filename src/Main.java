5ee44f731649c83f01287eb413d35021323be31e
Alex Mariakakis <atm15@duke.edu>
2012-02-22 12:26:32 -0500

    first attempt at conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import html_generator.HtmlConflictTable;
 import html_generator.HtmlGenerator;
 import html_generator.HtmlSortedList;
 import html_generator.HtmlTableCalendar;
@@ -11,32 +12,32 @@ import org.joda.time.DateTime;
 import parser.NFLCalParser;
 import parser.TivooParser;
 import event.CalendarEvent;
-import filter.FilterByKeywords;
 
 public class Main {
 	public static void main(String[] args) throws IOException, JDOMException {
 
 		// this is the new parser
-		TivooParser calendarParser = new NFLCalParser("NFL.xml");
+		TivooParser calendarParser = new NFLCalParser("dukecal.xml");
 		ArrayList<CalendarEvent> list = calendarParser.parseFile();
 
 		// testing filter
-		ArrayList<String> test = new ArrayList<String>();
-		test.add("Giants");
-		
-		FilterByKeywords f = new FilterByKeywords();
-		ArrayList<CalendarEvent> results = f.filter(list, test);
+//		ArrayList<String> test = new ArrayList<String>();
+//		test.add("Giants");
+//		
+//		FilterByKeywords f = new FilterByKeywords();
+//		ArrayList<CalendarEvent> results = f.filter(list, test);
 		
 //		FilterByDates f = new FilterByDates();
 //		ArrayList<CalendarEvent> results = f.filter(list, new DateTime(2010, 11,
 //		 11, 0, 0), new DateTime(2012, 12, 1, 23, 0));
+		ArrayList<CalendarEvent> results = list;
 
 		// html writing
-		String outputOption = "calendar";
+		String outputOption = "conflict";
 		
 		HtmlGenerator sortedlistCreator = new HtmlSortedList(results,
 				null, null);
-		HtmlGenerator conflictCreator = new HtmlTableCalendar(results, 
+		HtmlGenerator conflictCreator = new HtmlConflictTable(results, 
 				null, null);
 		HtmlGenerator calendarCreator = new HtmlTableCalendar(results, 
 				new DateTime(2011, 11, 30, 0, 0), new DateTime(2011, 12, 1, 23, 0));
