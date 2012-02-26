35fbe14784c9546fdf134a096a7273265f2530a7
lenaj15 <lj62@duke.edu>
2012-02-26 02:16:31 -0500

    able to filter events that do not contain given keywords

--- src/filter/FilterByDates.java
+++ src/filter/FilterByDates.java
@@ -7,11 +7,13 @@ import event.CalendarEvent;
 
 
 public class FilterByDates extends Filter {
-	//The map key will be the starting time of the event
-	public ArrayList<CalendarEvent> filter(ArrayList <CalendarEvent> list, DateTime start, DateTime end) {
+	//The parameter "include": true if user wants the events within the time frame to be included, false if user wants events
+	public ArrayList<CalendarEvent> filter(ArrayList <CalendarEvent> list, DateTime start, DateTime end, boolean include) {
 		
 		for (CalendarEvent e: list) {
-			if (e.getmyStartTime().getMillis() >= start.getMillis() && e.getmyEndTime().getMillis() <= end.getMillis())
+			
+			boolean condition = (e.getmyStartTime().getMillis() >= start.getMillis() && e.getmyEndTime().getMillis()<= end.getMillis());
+			if ((condition && include) || !(condition || include))
 				e.toggleIntersect();
 			else
 				e.setisOutput(false);
--- src/filter/FilterByKeywords.java
+++ src/filter/FilterByKeywords.java
@@ -5,12 +5,15 @@ import event.CalendarEvent;
 
 
 public class FilterByKeywords extends Filter {
-	//adds an event if it matches all the keywords
-	public ArrayList<CalendarEvent> filter(ArrayList <CalendarEvent> list, ArrayList<String> wordList) {
+	
+	//adds an event if it matches all the keywords unless the boolean 'include' is false
+	public ArrayList<CalendarEvent> filter(ArrayList <CalendarEvent> list, ArrayList<String> wordList, boolean include) {
 		
 		for (CalendarEvent e: list) {
 			for (String s : wordList) {
-				if (e.getMyTitle().contains(s))
+
+				boolean condition = e.getMyTitle().contains(s);
+				if ((condition && include) || !(condition || include))			
 					e.toggleIntersect();
 				else
 					e.setisOutput(false);
