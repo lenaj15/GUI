3f45c9e92847662d4d47a8974718067c96317529
Bryan <byang223@gmail.com>
2012-02-27 19:48:24 -0500

    commented Calendar event for easier using

--- src/event/CalendarEvent.java
+++ src/event/CalendarEvent.java
@@ -9,66 +9,91 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-
+/***
+ * Calendar Event class containing information about parsed calendar events
+ * 
+ * @author bryanyang
+ * 
+ */
 public class CalendarEvent {
-	
+
 	protected String myTitle, mySummaries;
 	protected DateTime myStartTime, myEndTime;
 	protected boolean isOutput;
 	protected HashMap<String, ArrayList<String>> myAttributes;
 
-	public CalendarEvent(String title, DateTime start, DateTime end, String summaries) {	
+	public CalendarEvent(String title, DateTime start, DateTime end,
+	        String summaries) {
 		myTitle = title;
 		myStartTime = start;
 		myEndTime = end;
-		mySummaries = summaries;	
+		mySummaries = summaries;
 		isOutput = true;
 		myAttributes = new HashMap<String, ArrayList<String>>();
 	}
-	
-	public void addAttribute(String attribute, String value){
-		if(myAttributes.keySet().contains(attribute)){
+
+	/***
+	 * This method is used to add specific values of specific attributes to put
+	 * into map
+	 * 
+	 * @param attribute
+	 * @param value
+	 */
+	public void addAttribute(String attribute, String value) {
+		if (myAttributes.keySet().contains(attribute)) {
 			myAttributes.get(attribute).add(value);
-		}else{
+		} else {
 			ArrayList<String> temp = new ArrayList<String>();
 			temp.add(value);
 			myAttributes.put(attribute, temp);
 		}
 	}
-	
-	public HashMap<String, ArrayList<String>> getAttributeMap(){
+
+	/***
+	 * This method returns the entire attribute map
+	 * @return
+	 */
+	public HashMap<String, ArrayList<String>> getAttributeMap() {
 		return myAttributes;
 	}
-	
-	public ArrayList<String> getAttributeValue(String s){
+
+	/***
+	 * This method returns the set of strings associated with 
+	 * a specific attribute
+	 * @param s
+	 * @return
+	 */
+	public ArrayList<String> getAttributeValue(String s) {
 		return myAttributes.get(s);
 	}
 
 	public void setisOutput(boolean value) {
-    	isOutput = value;
-    }
+		isOutput = value;
+	}
 
 	public void toggleIntersect() {
-    	isOutput &= true;
-    }
+		isOutput &= true;
+	}
 
 	public String getMyTitle() {
-    	return myTitle;
-    }
+		return myTitle;
+	}
 
 	public boolean getisOutput() {
-    	return isOutput;
-    }
+		return isOutput;
+	}
+
 	public DateTime getmyStartTime() {
-    	return myStartTime;
-    }
+		return myStartTime;
+	}
+
 	public DateTime getmyEndTime() {
-    	return myEndTime;
-    }
+		return myEndTime;
+	}
 
 	public String getMySummaries() {
-    	return mySummaries;
-    }
+		return mySummaries;
+	}
 
 	public String getMyDatesString() {
 		DateTimeFormatter fmt = DateTimeFormat.longDateTime();
@@ -76,46 +101,47 @@ public class CalendarEvent {
 		String end = fmt.print(myEndTime);
 		return start + " - " + end;
 	}
-	
-	public String toString(){
-		return "Title: " + myTitle + "\nDescription: " + mySummaries + "\nStarted: " + myStartTime + "\nEnded: " + myEndTime;
+
+	public String toString() {
+		return "Title: " + myTitle + "\nDescription: " + mySummaries
+		        + "\nStarted: " + myStartTime + "\nEnded: " + myEndTime;
 	}
 
-    public enum sortType  {
-    ByTitle(new Comparator<CalendarEvent>() {
-        public int compare(CalendarEvent event1, CalendarEvent event2) {
-	        return event1.getMyTitle().compareTo(event2.getMyTitle());
-        }
-    }),
-    
-    ByStartTime(new Comparator<CalendarEvent>() {
-       public int compare(CalendarEvent event1, CalendarEvent event2) {
-          return event1.myStartTime.compareTo(event2.myStartTime);
-       }
-    }),
+	public enum sortType {
+		ByTitle(new Comparator<CalendarEvent>() {
+			public int compare(CalendarEvent event1, CalendarEvent event2) {
+				return event1.getMyTitle().compareTo(event2.getMyTitle());
+			}
+		}),
 
-    ByEndTime(new Comparator<CalendarEvent>() {
-       public int compare(CalendarEvent event1, CalendarEvent event2) {
-          return event1.myEndTime.compareTo(event2.myEndTime);
-       }
-    });
-    private Comparator <CalendarEvent> myComparator;
+		ByStartTime(new Comparator<CalendarEvent>() {
+			public int compare(CalendarEvent event1, CalendarEvent event2) {
+				return event1.myStartTime.compareTo(event2.myStartTime);
+			}
+		}),
 
-    private sortType(Comparator<CalendarEvent> comparator){
-    	myComparator = comparator;
-    }
-    
-    public Comparator<CalendarEvent> ascending() {
-       return myComparator;
-    }
-    
-    public Comparator<CalendarEvent> descending() {
-       return Collections.reverseOrder(myComparator);
-    }
-    
-    public Comparator<CalendarEvent> getComparator(){
-    	return myComparator;
-    }
- }
-	
+		ByEndTime(new Comparator<CalendarEvent>() {
+			public int compare(CalendarEvent event1, CalendarEvent event2) {
+				return event1.myEndTime.compareTo(event2.myEndTime);
+			}
+		});
+		private Comparator<CalendarEvent> myComparator;
+
+		private sortType(Comparator<CalendarEvent> comparator) {
+			myComparator = comparator;
+		}
+
+		public Comparator<CalendarEvent> ascending() {
+			return myComparator;
+		}
+
+		public Comparator<CalendarEvent> descending() {
+			return Collections.reverseOrder(myComparator);
+		}
+
+		public Comparator<CalendarEvent> getComparator() {
+			return myComparator;
+		}
+	}
+
 }
